19c94af838e18ea27c4a33a6ce830ce6e5bdc112
Lucas Borges <lucasfabriciop@gmail.com>
2024-12-05 17:58:22 -0300

    oauth fixed

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -121,7 +121,7 @@ export default function Auth() {
                 supabase.auth.signInWithOAuth({
                   provider: 'google',
                   options: {
-                    redirectTo: `${window.location.origin}/auth/callback`,
+                    redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
                     queryParams: {
                       access_type: 'offline',
                       prompt: 'consent'
